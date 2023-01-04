40358bddec49574e945a7435b2004775e7b2ec5e
stefano lalli <stefano@dnasoft.nl>
2023-01-04 11:31:55 +0100

    Initial commit

--- src/extendMprojects.m
+++ src/extendMprojects.m
@@ -62,7 +62,7 @@ askAgainAction
 	;
 	; Populate the definition table according to the replaceType
 	set cnt=0
-	for  set cnt=cnt+1,line=$text(+cnt) quit:line=""  do:$find(line,";;")
+	for  set cnt=cnt+1,line=$text(+cnt) w !,"line:",line quit:line=""  do:$find(line,";;")
 	. set type=$piece(line," ",3),from=$piece(line," ",4),to=$piece(line," ",5)
 	. set:type'="" defs(type,from)=$select(replaceType="L":$$FUNC^%LCASE(to),replaceType="U":$$FUNC^%UCASE(to),1:to)
 	. set:type'="" defs(type,$$FUNC^%LCASE(from))=$select(replaceType="L":$$FUNC^%LCASE(to),replaceType="U":$$FUNC^%UCASE(to),1:to)
@@ -100,8 +100,8 @@ askAgainAction
 	;
 	;
 extendFile(buffer,defs)
-	new bufferAfter,cnt,line,cmd,lastPos,foundPos,match,newLine
-	new charCnt,char,charQuit
+	new bufferAfter,cnt,line,cmd,lastPos,foundPos,match,newLine,newLine2
+	new charCnt,char,charQuit,ifn
 	;
 	set cnt=0
 	for  set cnt=$order(buffer(cnt)) quit:cnt=""  do
@@ -110,13 +110,11 @@ extendFile(buffer,defs)
 	. ; left margin
 	. ; -------------------------
 	. if marginType>0 do
-	. . w !,$a($extract(line,1,1))
 	. . if $extract(line,1,1)'=" ",$extract(line,1,1)'=$char(9) quit
 	. . set charQuit=0
 	. . for charCnt=1:1:$length(line) do  quit:charQuit
 	. . . set char=$extract(line,charCnt,charCnt)
 	. . . if char'=" ",char'=$char(9) set charQuit=charCnt
-	. . w !,"charQuit:",charQuit
 	. . set line=$select(marginType=1:" ",marginType=4:"    ",1:" ")_$extract(line,charQuit,$length(line))
 	. ; -------------------------
 	. ; commands
@@ -124,20 +122,21 @@ extendFile(buffer,defs)
 	. set (cmd,newLine)=""
 	. for  set cmd=$order(defs("CMD",cmd)) quit:cmd=""  do
 	. . set foundPos=0,lastPos=1,match=""
-	. . for  set foundPos=$find(line,cmd,lastPos) quit:foundPos=0  do
-	. . . set match=$extract(line,foundPos-1)
+	. . for  set foundPos=$find(line,cmd_" ",lastPos) quit:foundPos=0  do
+	. . . set match=$extract(line,foundPos-2)
+	. . . WRITE !,"*MATCH:",match
 	. . . ; is next char either a space/tab or a : AND (is it first char OR prev char is space/tab)
-	. . . if $extract(line,foundPos)=" "!($extract(line,foundPos)=$char(9))!($extract(line,foundPos)=":"),lastPos=2!($extract(line,foundPos-2)=" ")!($extract(line,foundPos-2)=$char(9)) do
+	. . . if $extract(line,foundPos-1)=" "!($extract(line,foundPos-1)=$char(9))!($extract(line,foundPos-1)=":"),lastPos=2!($extract(line,foundPos-3)=" ")!($extract(line,foundPos-3)=$char(9)) do
 	. . . . ; Replace
-	. . . . set newLine=newLine_$extract(line,lastPos,foundPos-2)_defs("CMD",cmd)
+	. . . . set newLine=newLine_$extract(line,lastPos,foundPos-3)_defs("CMD",cmd)_" "
 	. . . . set line=$extract(line,foundPos,$length(line))
 	. . . ;
 	. . . set lastPos=foundPos
 	. ;
 	. if newLine="" set newLine=line
 	. else  if $length(line)'=$length(newLine) set newLine=newLine_$extract(line,foundPos,$length(line))
-	. set bufferAfter(cnt)=newLine
 	. ;
+	. set bufferAfter(cnt)=newLine
 	;	
 	quit *bufferAfter
 	;
@@ -148,8 +147,49 @@ extendFile(buffer,defs)
 	;; CMD B BREAK
 	;; CMD C CLOSE
 	;; CMD D DO
+	;; CMD E ELSE
+	;; CMD F FOR
+	;; CMD G GOTO
+	;; CMD H HANG
+	;; CMD I IF
+	;; CMD J JOB
+	;; CMD K KILL
+	;; CMD L LOCK
+	;; CMD M MERGE
+	;; CMD N NEW
+	;; CMD O OPEN
+	;; CMD Q QUIT
+	;; CMD R READ
 	;; CMD S SET
+	;; CMD TC TCOMMIT
+	;; CMD TRE TRESTART
+	;; CMD TRO TROLLBACK
+	;; CMD TS TSTART
+	;; CMD U USE
+	;; CMD V VIEW
 	;; CMD W WRITE
+	;; CMD X XECUTE
+	;; CMD ZA ZALLOCATE
+	;; CMD ZB ZBREAK
+	;; CMD ZCOM ZCOMPILE
+	;; CMD ZC ZCONTINUE
+	;; CMD ZD ZDEALLOCATE
+	;; CMD ZE ZEDIT
+	;; CMD ZG ZGOTO
+	;; CMD ZH ZHELP
+	;; CMD ZK ZKILL
+	;; CMD ZL ZLINK
+	;; CMD ZM ZMESSAGE
+	;; CMD ZP ZPRINT
+	;; CMD ZRU ZRUPDATE
+	;; CMD ZS ZSHOW
+	;; CMD ZST ZSTEP
+	;; CMD ZSY ZSYSTEM
+	;; CMD ZTC ZTCOMMIT
+	;; CMD ZTR ZTRIGGER
+	;; CMD ZTS ZTSTART
+	;; CMD ZW ZWITHDRAW
+	;; CMD ZWR ZWRITE
 	;; IFN A ASCII
 	;; IFN C CHAR
 	;; ISV D DEVICE
--- src/test.m
+++ src/test.m
@@ -2,6 +2,7 @@ TEST
     s a=0,c=20 w "test"
    w "test"
    write "a"
+   ZWR TEST
   CLOSE s
 ; comment
     c s
