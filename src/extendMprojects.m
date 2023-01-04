d5701ac8717a760a14169617eafb06ed05378771
stefano lalli <stefano@dnasoft.nl>
2023-01-04 15:15:31 +0100

    Initial commit

--- src/extendMprojects.m
+++ src/extendMprojects.m
@@ -20,7 +20,7 @@ start(sourceDir)
 	write !
 	;
 	new file,files,replaceType,marginType,cnt,inputStr,defs,line,action
-	new bufferIn,bufferOut,cmd,ifn,isv
+	new bufferIn,bufferOut,cmd,ifn,isv,quitNow,extension
 	;
 	; Check # of files in dir
 	if $extract(sourceDir,$length(sourceDir))'="/" set sourceDir=sourceDir_"/"
@@ -44,11 +44,11 @@ askAgainType
 	;
 	; Prompt for left margin
 askAgainMargin
-	read !!,"How do you want to convert the left margin ? (A)s it is, (1) space, (4) spaces, (Q)uit ",inputStr
+	read !!,"How do you want to convert the left margin ? (A)s it is, (1) space, (4) spaces, (T)tab, (Q)uit ",inputStr
 	set inputStr=$$FUNC^%UCASE(inputStr)
 	if inputStr="Q" do  quit
 	. write !!,"Bye bye",!!
-	if inputStr="A"!(inputStr="1")!(inputStr="4") set marginType=$select(inputStr="A":0,1:+inputStr)
+	if inputStr="A"!(inputStr="1")!(inputStr="4")!(inputStr="T") set marginType=$select(inputStr="A":0,inputStr="T":"T",1:+inputStr)
 	else  goto askAgainMargin
 	;
 	; Prompt for the action
@@ -60,9 +60,36 @@ askAgainAction
 	if inputStr="R"!(inputStr="A")!(inputStr="W") set action=inputStr
 	else  goto askAgainAction
 	;
+	if action="R" goto proceed
+	;
+aksAgainExtension
+	read !!,"Enter the extension you wish to have appended to the .m files: ",!,"It must start with a ., type (Q) to quit ",inputStr
+	if inputStr="Q"!(inputStr="q") do  quit
+	. write !!,"Bye bye",!!
+	if $extract(inputStr)="" goto aksAgainExtension
+	if $extract(inputStr,1,1)'="." goto aksAgainExtension
+	if $length(inputStr)<2 goto aksAgainExtension
+	set extension=inputStr
+	;
+proceed
+	; Overview
+	write !!,"OVERVIEW",!
+	write !,"You will convert the file into extended "_$select(replaceType="L":"lower case",1:"UPPER CASE")
+	write !,"The left margin will be "_$select(marginType=0:"left as it is",marginType="T":"a tab character",marginType=4:"4 spaces",1:"1 space")
+	if action="A" write !,"The newly formatted files will replace the existing files, while the old files with have the extension .m"_extension
+	if action="W" write !,"The newly formatted files will will have the extension .m"_extension_" while the old files won't be changed."
+	if action="R" write !,"The original files will be overwritten with the new files"
+	write !,"A total of "_files_" file"_$select(files=1:"",1:"s")_" will be processed."
+	;
+	read !!,"Do you want to proceed ? (Y)es, (N)o, (Q)uit ",inputStr
+	set inputStr=$$FUNC^%UCASE(inputStr)
+	if inputStr="Q"!(inputStr="N") do  quit
+	. write !!,"Bye bye",!!
+	if inputStr'="Y" goto proceed
+	;
 	; Populate the definition table according to the replaceType
 	set cnt=0
-	for  set cnt=cnt+1,line=$text(+cnt) w !,"line:",line quit:line=""  do:$find(line,";;")
+	for  set cnt=cnt+1,line=$text(+cnt) quit:line=""  do:$find(line,";;")
 	. set type=$piece(line," ",3),from=$piece(line," ",4),to=$piece(line," ",5)
 	. set:type'="" defs(type,from)=$select(replaceType="L":$$FUNC^%LCASE(to),replaceType="U":$$FUNC^%UCASE(to),1:to)
 	. set:type'="" defs(type,$$FUNC^%LCASE(from))=$select(replaceType="L":$$FUNC^%LCASE(to),replaceType="U":$$FUNC^%UCASE(to),1:to)
@@ -78,10 +105,6 @@ askAgainAction
 	set isv="" for  set isv=$order(defs("ISV",isv)) quit:isv=""  do
 	. set defs("replace","isv",isv_"=")=defs("ISV",isv)_"="
 	. set defs("replace","isv","="_isv)="="_defs("ISV",isv)
-	;
-	write !!,cnt,!
-	zwr defs
-	write !
 	;
 	; ----------------------------------------------
 	; We can start the conversion
@@ -118,7 +141,6 @@ extendFile(buffer,defs)
 	merge cmdReplace=defs("replace","cmd")
 	merge ifnReplace=defs("replace","ifn")
 	merge isvReplace=defs("replace","isv")
-	zwr isvReplace
 	;
 	set cnt=0
 	for  set cnt=$order(buffer(cnt)) quit:cnt=""  do
@@ -126,13 +148,13 @@ extendFile(buffer,defs)
 	. ; -------------------------
 	. ; left margin
 	. ; -------------------------
-	. if marginType>0 do
+	. if marginType>0!(marginType="T") do
 	. . if $extract(line,1,1)'=" ",$extract(line,1,1)'=$char(9) quit
 	. . set charQuit=0
 	. . for charCnt=1:1:$length(line) do  quit:charQuit
 	. . . set char=$extract(line,charCnt,charCnt)
 	. . . if char'=" ",char'=$char(9) set charQuit=charCnt
-	. . set line=$select(marginType=1:" ",marginType=4:"    ",1:" ")_$extract(line,charQuit,$length(line))
+	. . set line=$select(marginType="T":$char(9),marginType=1:" ",marginType=4:"    ",1:" ")_$extract(line,charQuit,$length(line))
 	. ; -------------------------
 	. ; commands
 	. ; -------------------------
@@ -142,9 +164,34 @@ extendFile(buffer,defs)
 	. ; commands
 	. ; -------------------------
 	. set line=$$STRRPLC(line,.isvReplace)
+	. ;
 	. set bufferAfter(cnt)=line
 	;
 	quit *bufferAfter
+	;
+	;
+STRRPLC(IN,SPEC) ;
+	Q:'$D(IN) "" 
+	Q:$D(SPEC)'>9 IN 
+	;
+	N A1,A2,A3,A4,A5,A6,A7,A8
+	;
+	S A1=$L(IN),A7=$J("",A1),A3="",A6=9999 
+	;
+	F  S A3=$O(SPEC(A3)) Q:A3=""  S A6(A6)=A3,A6=A6-1
+	F A6=0:0 S A6=$O(A6(A6)) Q:A6'>0  S A3=A6(A6) D:$D(SPEC(A3))#2 RE1
+	S A8="" F A2=1:1:A1 D RE3
+	;
+	Q A8
+	;
+RE1 S A4=$L(A3),A5=0 F  S A5=$F(IN,A3,A5) Q:A5<1  D RE2
+	Q
+RE2 Q:$E(A7,A5-A4,A5-1)["X"  S A8(A5-A4)=SPEC(A3)
+	F A2=A5-A4:1:A5-1 S A7=$E(A7,1,A2-1)_"X"_$E(A7,A2+1,A1)
+	Q
+RE3 I $E(A7,A2)=" " S A8=A8_$E(IN,A2) Q
+	S:$D(A8(A2)) A8=A8_A8(A2)
+	Q
 	;
 	;
 	; ---------------------------
@@ -238,20 +285,6 @@ extendFile(buffer,defs)
 	;
 	;
 	;
-STRRPLC(IN,SPEC) ;
-	Q:'$D(IN) "" Q:$D(SPEC)'>9 IN N A1,A2,A3,A4,A5,A6,A7,A8
-	S A1=$L(IN),A7=$J("",A1),A3="",A6=9999 F  S A3=$O(SPEC(A3)) Q:A3=""  S A6(A6)=A3,A6=A6-1
-	F A6=0:0 S A6=$O(A6(A6)) Q:A6'>0  S A3=A6(A6) D:$D(SPEC(A3))#2 RE1
-	S A8="" F A2=1:1:A1 D RE3
-	Q A8
 	;
-RE1 S A4=$L(A3),A5=0 F  S A5=$F(IN,A3,A5) Q:A5<1  D RE2
-	Q
-RE2 Q:$E(A7,A5-A4,A5-1)["X"  S A8(A5-A4)=SPEC(A3)
-	F A2=A5-A4:1:A5-1 S A7=$E(A7,1,A2-1)_"X"_$E(A7,A2+1,A1)
-	Q
-RE3 I $E(A7,A2)=" " S A8=A8_$E(IN,A2) Q
-	S:$D(A8(A2)) A8=A8_A8(A2)
-	Q
 	;
 	;
